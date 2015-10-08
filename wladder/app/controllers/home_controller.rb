39da133e3bb428f75cc87b6ec2b5001a6dca17fb
mlizama <mlizama@mail.csuchico.edu>
2015-10-08 11:36:23 -0700

    Finished

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -8,8 +8,42 @@ class HomeController < ApplicationController
 	
 	def show
 
+	@iter = 1
+
+	@myarray = []
+
 	@begin = params[:begin]
-	@end   = params[:end]	
-	end
+	@myarray.push(@begin)
+
+	@one   = params[:one]
+	@myarray.push(@one)
+
+	@two   = params[:two]
+	@myarray.push(@two)	
+
+	@three = params[:three]
+	@myarray.push(@three)
+
+	@four  = params[:four]
+        @myarray.push(@four)
+
+	@five  = params[:five]	
+        @myarray.push(@five)
+
+
+        @end   = params[:end]
+        @myarray.push(@end)
+
+	@myarray.delete("")
+
+	@win = legal @myarray
+	@result = ""
+	
+	   if @win
+	   @result = "Hurray you won!"
+	   else
+	   @result = "Sorry you lost!"
+	   end
+	end		
 
 end
