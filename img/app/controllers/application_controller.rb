c8ad6417d1337cf9d156c0d4315342e65eb26fd0
mlizama <mlizama@mail.csuchico.edu>
2015-11-15 18:16:26 -0800

    user authentication done!

--- img/app/controllers/application_controller.rb
+++ img/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+#before_filter :authenticate_user!
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- img/config/environments/production.rb
+++ img/config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+# enable mailer so Devise can send email
+  config.action_mailer.default_url_options = { :moises465 => 'http://ec2-54-69-30-218.us-west-2.compute.amazonaws.com/ratings' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # Disable delivery errors, bad email addresses will be ignored
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
 end
