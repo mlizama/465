3d5b74ee379f269e665b2d6b2fb6f6eff84f1ad5
mlizama <mlizama@mail.csuchico.edu>
2015-12-11 16:30:23 -0800

    got bitcoin prices showin up

--- coin/app/controllers/home_controller.rb
+++ coin/app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
+require 'coinbase/wallet'
+
 class HomeController < ApplicationController
+
+def index
+key = "oHQ2zQEHJkms7HYV"
+secret = "mvmhft7dI5m7rL0X2etTLb2mDnuVhX6M"
+client = Coinbase::Wallet::Client.new(api_key: key, api_secret: secret, api_url: "https://api.sandbox.coinbase.com")
+
+@price = client.buy_price
+
+end
+
 end
