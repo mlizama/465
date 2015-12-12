902945c76c14ea3bc7d7a3b0fb9db81a7515a070
mlizama <mlizama@mail.csuchico.edu>
2015-12-11 23:02:04 -0800

    added account table to hold wallet

--- coin/app/controllers/home_controller.rb
+++ coin/app/controllers/home_controller.rb
@@ -3,13 +3,15 @@ require 'coinbase/wallet'
 class HomeController < ApplicationController
 
 def index
-key = "oHQ2zQEHJkms7HYV"
-secret = "mvmhft7dI5m7rL0X2etTLb2mDnuVhX6M"
+key = "a4bpRUI0lIAiFWHL"
+secret = "nmX8ry1OicN52o2tkgnX9GnAv0OS86o8"
 @client = Coinbase::Wallet::Client.new(api_key: key, api_secret: secret, api_url: "https://api.sandbox.coinbase.com")
 @price = @client.buy_price
 @amount = @price['amount']
+@accounts = @client.accounts
+@address = @accounts[2]
 
-
+@client.create_account(name: "moises")
 end
 
 end
--- coin/config/routes.rb
+++ coin/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :accounts
   devise_for :users
 
-  root 'home#index'
+  root 'accounts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
