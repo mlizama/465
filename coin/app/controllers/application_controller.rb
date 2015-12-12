40c1c2669e56045c83fd78d5bb9d7e5e55ca8e12
mlizama <mlizama@mail.csuchico.edu>
2015-12-11 17:17:09 -0800

    getting bitcoin prices from my sandbox account

--- coin/app/controllers/application_controller.rb
+++ coin/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
+require 'coinbase/wallet'
+
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!
--- coin/app/controllers/home_controller.rb
+++ coin/app/controllers/home_controller.rb
@@ -5,9 +5,10 @@ class HomeController < ApplicationController
 def index
 key = "oHQ2zQEHJkms7HYV"
 secret = "mvmhft7dI5m7rL0X2etTLb2mDnuVhX6M"
-client = Coinbase::Wallet::Client.new(api_key: key, api_secret: secret, api_url: "https://api.sandbox.coinbase.com")
+@client = Coinbase::Wallet::Client.new(api_key: key, api_secret: secret, api_url: "https://api.sandbox.coinbase.com")
+@price = @client.buy_price
+@amount = @price['amount']
 
-@price = client.buy_price
 
 end
 
